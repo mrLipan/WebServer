79ad390d4e9caf37d03e9a3111a341df6fcb1afc
unknown <pl4748976@gmail.com>
2025-11-06 00:19:21 +0800

    EventLoopThread

---
+++ net/EventLoopThread.cpp
@@ -0,0 +1,47 @@
+#include "EventLoopThread.h"
+
+EventLoopThread::EventLoopThread(const ThreadInitCallback& cb,
+                                 const string& name)
+    : loop_(NULL),
+      exiting_(false),
+      thread_(bind(&EventLoopThread::threadFunc, this), name),
+      mutex_(),
+      cond_(mutex_),
+      callback_(cb) {}
+
+EventLoopThread::~EventLoopThread() {
+  exiting_ = true;
+  if (loop_ != NULL) {
+    loop_->quit();
+    thread_.join();
+  }
+}
+
+EventLoop* EventLoopThread::startLoop() {
+  assert(!thread_.started());
+  thread_.start();
+  {
+    MutexLockGuard lock(mutex_);
+
+    while (loop_ == NULL) cond_.wait();
+  }
+  return loop_;
+}
+
+void EventLoopThread::threadFunc() {
+  EventLoop loop;
+
+  if (callback_) {
+    callback_(&loop);
+  }
+  {
+    MutexLockGuard lock(mutex_);
+    loop_ = &loop;
+    cond_.notify();
+  }
+
+  loop.loop();
+
+  MutexLockGuard lock(mutex_);
+  loop_ = NULL;
+}
