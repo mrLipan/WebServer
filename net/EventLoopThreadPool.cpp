d84deb4614bb1f52b62e177f892743c1fbb50663
unknown <pl4748976@gmail.com>
2025-11-06 04:38:25 +0800

    EventLoopThreadPool

---
+++ net/EventLoopThreadPool.cpp
@@ -0,0 +1,49 @@
+#include "EventLoopThreadPool.h"
+
+EventLoopThreadPool::EventLoopThreadPool(EventLoop* baseLoop,
+                                         const string& name)
+    : baseLoop_(baseLoop),
+      name_(name),
+      started_(false),
+      numThreads_(0),
+      next_(0) {}
+
+EventLoopThreadPool::~EventLoopThreadPool() {}
+
+void EventLoopThreadPool::start(const ThreadInitCallback& cb) {
+  assert(!started_);
+  // assertInLoopThread 保证只能由主线程调用
+  baseLoop_->assertInLoopThread();
+  started_ = true;
+  for (int i = 0; i < numThreads_; ++i) {
+    char buf[name_.size() + 32];
+    snprintf(buf, sizeof(buf), "%s%d", name_.c_str(), i);
+    std::unique_ptr<EventLoopThread> t(new EventLoopThread(cb, buf));
+    threads_.push_back(t);
+    loops_.push_back(t->startLoop());
+  }
+  if (numThreads_ == 0 && cb) {
+    cb(baseLoop_);
+  }
+}
+
+EventLoop* EventLoopThreadPool::getNextLoop() {
+  baseLoop_->assertInLoopThread();
+  assert(started_);
+  EventLoop* loop = baseLoop_;
+  if (!loops_.empty()) {
+    loop = loops_[next_];
+    next_ = (next_ + 1) % numThreads_;
+  }
+  return loop;
+}
+
+std::vector<EventLoop*> EventLoopThreadPool::getAllLoops() {
+  baseLoop_->assertInLoopThread();
+  assert(started_);
+  if (loops_.empty()) {
+    return std::vector<EventLoop*>(1, baseLoop_);
+  } else {
+    return loops_;
+  }
+}
---
+++ net/EventLoopThreadPool.h
@@ -0,0 +1,37 @@
+#ifndef EVENTLOOPTHREADPOOL_H
+#define EVENTLOOPTHREADPOOL_H
+
+#include <memory>
+#include <vector>
+
+#include "EventLoopThread.h"
+#include "base/Logging.h"
+#include "base/noncopyable.h"
+
+class EventLoopThreadPool : noncopyable {
+ public:
+  typedef std::function<void(EventLoop*)> ThreadInitCallback;
+  EventLoopThreadPool(EventLoop* baseLoop, const string& name);
+
+  ~EventLoopThreadPool();
+  void setThreadNum(int numThreads) { numThreads_ = numThreads; }
+  void start(const ThreadInitCallback& cb = ThreadInitCallback());
+
+  EventLoop* getNextLoop();
+  std::vector<EventLoop*> getAllLoops();
+
+  bool started() const { return started_; }
+
+  const string& name() const { return name_; }
+
+ private:
+  EventLoop* baseLoop_;
+  string name_;
+  bool started_;
+  int numThreads_;
+  int next_;
+  std::vector<std::unique_ptr<EventLoopThread>> threads_;
+  std::vector<EventLoop*> loops_;
+};
+
+#endif
