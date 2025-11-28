8c03042d3b9b6119f6ededad3cde25ae8c3e4d58
unknown <pl4748976@gmail.com>
2025-11-28 16:37:17 +0800

    condition

---
+++ base/Condition.h
@@ -0,0 +1,33 @@
+#ifndef CONDITION_H
+#define CONDITION_H
+
+#include <errno.h>
+#include <pthread.h>
+#include <time.h>
+
+#include "Mutex.h"
+#include "noncopyable.h"
+
+class Condition : noncopyable {
+ public:
+  explicit Condition(MutexLock& _mutex) : mutex(_mutex) {
+    pthread_cond_init(&cond, NULL);
+  }
+  ~Condition() { pthread_cond_destroy(&cond); }
+  void wait() { pthread_cond_wait(&cond, mutex.get()); }
+  void notify() { pthread_cond_signal(&cond); }
+  void notifyAll() { pthread_cond_broadcast(&cond); }
+  bool waitForSeconds(int seconds)
+  {
+    struct timespec abstime;
+    clock_gettime(CLOCK_REALTIME, &abstime);
+    abstime.tv_sec += static_cast<time_t>(seconds);
+    return ETIMEDOUT == pthread_cond_timedwait(&cond, mutex.get(), &abstime);
+  }
+
+ private:
+  MutexLock& mutex;
+  pthread_cond_t cond;
+};
+
+#endif  // CONDITION_H
