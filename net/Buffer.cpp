c80e4815ccff7aa38729e5e4eaeb4231dc9b8a8f
mrLipan <73480102+mrLipan@users.noreply.github.com>
2025-11-28 17:55:51 +0800

    Update Buffer.cpp
    修改为边沿触发

--- net/Buffer.cpp
+++ net/Buffer.cpp
@@ -17,7 +17,7 @@ ssize_t Buffer::readFd(int fd, int* savedErrno)
   vec[0].iov_len = writable;
   vec[1].iov_base = extrabuf;
   vec[1].iov_len = sizeof(extrabuf);
-  // 水平触发 + 一次性尽可能读取多的数据
+  // 一次性尽可能读取多的数据
   const int iovcnt = (writable < sizeof(extrabuf)) ? 2 : 1;
   const ssize_t n = readv(fd, vec, iovcnt);
   if (n < 0)
