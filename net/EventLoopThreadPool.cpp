bfd30195c58275eca815e59f176776fd65b8650e
mrLipan <73480102+mrLipan@users.noreply.github.com>
2026-01-06 22:09:44 +0800

    Update EventLoopThreadPool.cpp

--- net/EventLoopThreadPool.cpp
+++ net/EventLoopThreadPool.cpp
@@ -7,56 +7,46 @@ EventLoopThreadPool::EventLoopThreadPool(EventLoop* baseLoop,
       name_(name),
       started_(false),
       numThreads_(numThreads),
-      next_(0)
-{
+      next_(0) {
   assert(numThreads_ >= 0);
 }
 
 EventLoopThreadPool::~EventLoopThreadPool() {}
 
-void EventLoopThreadPool::start(const ThreadInitCallback& cb)
-{
+void EventLoopThreadPool::start(const ThreadInitCallback& cb) {
   assert(!started_);
   // assertInLoopThread 保证只能由主线程调用
   baseLoop_->assertInLoopThread();
   started_ = true;
-  for (int i = 0; i < numThreads_; ++i)
-  {
+  for (int i = 0; i < numThreads_; ++i) {
     char buf[name_.size() + 32];
     snprintf(buf, sizeof(buf), "%s%d", name_.c_str(), i);
     EventLoopThread* t = new EventLoopThread(cb, buf);
     threads_.push_back(std::unique_ptr<EventLoopThread>(t));
     loops_.push_back(t->startLoop());
   }
-  if (numThreads_ == 0 && cb)
-  {
+  if (numThreads_ == 0 && cb) {
     cb(baseLoop_);
   }
 }
 
-EventLoop* EventLoopThreadPool::getNextLoop()
-{
+EventLoop* EventLoopThreadPool::getNextLoop() {
   baseLoop_->assertInLoopThread();
   assert(started_);
   EventLoop* loop = baseLoop_;
-  if (!loops_.empty())
-  {
+  if (!loops_.empty()) {
     loop = loops_[next_];
     next_ = (next_ + 1) % numThreads_;
   }
   return loop;
 }
 
-std::vector<EventLoop*> EventLoopThreadPool::getAllLoops()
-{
+std::vector<EventLoop*> EventLoopThreadPool::getAllLoops() {
   baseLoop_->assertInLoopThread();
   assert(started_);
-  if (loops_.empty()) 
-  {
+  if (loops_.empty()) {
     return std::vector<EventLoop*>(1, baseLoop_);
-  } 
-  else 
-  {
+  } else {
     return loops_;
   }
 }
